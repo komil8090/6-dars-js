9efa433c2df48310433298a16b13b747a600b3c0
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2024-06-02 13:38:00 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let son1 = Number(prompt("Son kriting"));
+let son2 = Number(prompt("Ikkinchi soni kriting"));
+
+let amal = prompt(`Amalni talang:
+1-qo'shish
+2-ayirish
+3-kopaytirish
+4-bolish
+amalni Tanlang:(1/2/3/4):
+`);
+
+
+if (amal === "1") {
+    alert(`${son1} + ${son2} = ${son1 + son2}`)
+} else if (amal === 2){
+    alert(`${son1} + ${son2} = ${son1 + son2}`)
+} else if (amal === 3){
+    alert(`${son1} + ${son2} = ${son1 + son2}`)
+} else if (amal === 4){
+    alert(`${son1} + ${son2} = ${son1 + son2}`)
+}
+
